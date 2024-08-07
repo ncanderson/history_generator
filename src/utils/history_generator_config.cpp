a346745d89724df29c793267417360f7778e5433
Nate Anderson <nanderson@greensea.com>
2024-08-07 11:51:45 -0400

    rename files

---
+++ src/utils/history_generator_config.cpp
@@ -0,0 +1,3 @@
+#include "history_generator_config_base.h"
+
+history_generator_config::history_generator_config() {}
---
+++ src/utils/history_generator_config.h
@@ -0,0 +1,27 @@
+#ifndef HISTORY_GENERATOR_CONFIG_H
+#define HISTORY_GENERATOR_CONFIG_H
+
+/**
+ * @brief Application config
+ * @details
+ */
+class history_generator_config
+{
+public:
+  // Attributes
+
+  // Implementation
+
+protected:
+  // Attributes
+
+  // Implementation
+
+private:
+  // Attributes
+
+  // Implementation
+
+}; // history_generator_config_base
+
+#endif // HISTORY_GENERATOR_CONFIG_H
