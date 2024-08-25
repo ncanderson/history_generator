ed34499a7046a36c6c2fda9726ba3e073f52eef9
Nate Anderson <nanderson@greensea.com>
2024-08-24 20:27:16 -0400

    add files for data access

---
+++ src/data_access/data_access_dao_base.h
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2024 Nate Anderson - All Rights Reserved
+ */
+
+#ifndef DATA_ACCESS_DAO_BASE_H
+#define DATA_ACCESS_DAO_BASE_H
+
+namespace his_gen
+{
+/**
+ * @brief The Data_access_dao_base class
+ */
+class Data_access_dao_base
+{
+public:
+  // Attributes
+
+  // Implementation
+  /**
+   * @brief Destructor
+   */
+  ~Data_access_dao_base();
+
+  /**
+   * @brief Virtual base for entity creation functions
+   */
+  virtual void Create_entity() = 0;
+
+protected:
+  // Attributes
+
+  // Implementation
+
+private:
+  // Attributes
+
+  // Implementation
+
+}; // class Data_access_dao_base
+}  // namespace his_gen
+
+#endif // DATA_ACCESS_DAO_BASE_H
---
+++ src/data_access/data_access_dao_file.h
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C) 2024 Nate Anderson - All Rights Reserved
+ */
+
+#ifndef DATA_ACCESS_DAO_FILE_H
+#define DATA_ACCESS_DAO_FILE_H
+
+// Application files
+#include <data_access/data_access_dao_base.h>
+
+namespace his_gen
+{
+/**
+ * @brief The Data_access_dao_file class
+ */
+class Data_access_dao_file : Data_access_dao_base
+{
+
+}; // class Data_access_dao_base
+}  // namespace his_gen
+
+#endif // DATA_ACCESS_DAO_FILE_H
