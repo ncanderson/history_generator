97e83d534efd1a93b696851d4a3b0a24074a8c31
Nate Anderson <nanderson@greensea.com>
2024-09-19 22:14:43 -0400

    add names class

---
+++ src/modules/names.h
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2024 Nate Anderson - All Rights Reserved
+ */
+
+#ifndef NAMES_H
+#define NAMES_H
+
+// Standard libs
+#include <string>
+
+// JSON
+#include <deps/json.hpp>
+
+// Application files
+
+namespace his_gen
+{
+/**
+ * @brief Names module
+ */
+class Names
+{
+public:
+  // Attributes
+
+  // Implementation
+  /**
+   * @brief Constructor
+   */
+  Names(){}
+
+  /**
+   * @brief Destructor
+   */
+  ~Names(){};
+
+protected:
+  // Attributes
+
+  // Implementation
+
+private:
+  // Attributes
+
+  // Implementation
+
+}; // class Names
+}  // namespace his_gen
+
+#endif // NAMES_H
