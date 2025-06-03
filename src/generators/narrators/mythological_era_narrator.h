1bbaa521ab3f84db331c4f996187b14db44219eb
Nate Anderson <nanderson@greensea.com>
2025-06-03 09:54:40 -0400

    add narrators for other eras

---
+++ src/generators/narrators/mythological_era_narrator.h
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2025 Nate Anderson - All Rights Reserved
+ */
+
+#ifndef MYTH_ERA_NARRATOR_H
+#define MYTH_ERA_NARRATOR_H
+
+// Standard libs
+
+// Application files
+#include <generators/narrators/narrator_base.h>
+
+namespace his_gen
+{
+/**
+ * @brief Myth era generator narrator
+ */
+class Myth_era_narrator : public Narrator_base
+{
+public:
+  // Attributes
+
+  // Implementation
+  /**
+   * @brief Constructor
+   */
+  Myth_era_narrator();
+
+  /**
+   * @brief Destructor
+   */
+  ~Myth_era_narrator();
+
+protected:
+  // Attributes
+
+  // Implementation
+
+private:
+  // Attributes
+
+  // Implementation
+
+}; // class Myth_era_narrator
+}  // namespace his_gen
+
+#endif // MYTH_ERA_NARRATOR_H
